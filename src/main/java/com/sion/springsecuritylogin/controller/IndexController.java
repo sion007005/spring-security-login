f48b2cecd61b38100b8ec26400c6dfc8bdeb8015
Sion <sion007005@gmail.com>
2022-01-06 21:17:31 +0900

    Initial commit

---
+++ src/main/java/com/sion/springsecuritylogin/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.sion.springsecuritylogin.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+}

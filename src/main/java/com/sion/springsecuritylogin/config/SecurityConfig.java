33aa114e1ffb5e0d3d5b6e7868b8884372fed09e
Sion <sion007005@gmail.com>
2022-01-06 21:38:43 +0900

    시큐리티 필터를 통해 페이지 접근 권한 설정

---
+++ src/main/java/com/sion/springsecuritylogin/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.sion.springsecuritylogin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록된다.
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin() // 권한이 없는 사람이 접근하면 로그인 페이지로 이동시킨다
+                .loginPage("/login");
+    }
+}
